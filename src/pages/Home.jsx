32ff1cbb92d57d831f79c06b5de447f3de40f98c
Vishal Jaiswal <kumardenzel@gmail.com>
2025-07-03 00:47:36 +0530

    herosection is coded

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { HeroSection } from '../components/HeroSection'
 
 export const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <HeroSection/>
+    </>
   )
 }
